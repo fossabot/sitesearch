d3365df48dd093e1a20f22a2f2964ba229bdb269
Dylan Tientcheu <dylantientcheu@gmail.com>
2025-10-16 10:04:05 +0200

    fix: remove sidepanel react

--- packages/sidepanel-react/src/index.ts
+++
@@ -1,7 +0,0 @@
-export type { AskAIConfig } from "./askai";
-export { ChatWidget } from "./chat";
-export { PanelHeader } from "./panel-header";
-export { default as PromptTextarea } from "./prompt-textarea";
-export type { SidepanelProps } from "./sidepanel";
-export { Sidepanel } from "./sidepanel";
-import "./styles.css";
